c3056b00928eb90cb314fcd22685354a62eb0087
Rashedul Kabir <rkabir.rashed@gmail.com>
2020-06-29 11:59:58 +0600

    folder structure created

---
+++ Assets/RandomMapGen/Scripts/Map.cs
@@ -0,0 +1,28 @@
+﻿// this class is to make the maps
+// we will use 1D array to store the maps
+// while 2D array is easier to represent, this makes the game consume more memory
+
+
+
+public class Map
+{
+    public Tile[] tiles;
+    public int columns; // will represent the width of our map
+    public int rows; // will represent the height of our map
+
+    public void NewMap(int width, int height)
+    {
+        columns = width;
+        rows = height;
+        
+        tiles = new Tile[columns*rows];
+    }
+    
+    
+    //method to create the tiles
+    private void CreateTiles()
+    {
+        
+    }
+    
+}
---
+++ Assets/RandomMapGen/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿// this class is to manage the tiles. we don't need to extend the regular unity classes
+
+
+
+public enum Sides
+{
+    Bottom,
+    Right,
+    Left,
+    Top
+}
+
+public class Tile
+{
+    public int id = 0;
+    public Tile[] neighbors = new Tile[4];
+
+}
